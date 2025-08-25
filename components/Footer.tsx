3b575b219dcd7af9a32d0059e84f07d016b45c49
meit <zhora.makarov.2005@yandex.ru>
2025-08-25 14:33:59 +0900

    footer black

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <footer className="bg-brand-dark text-white">
       <div className="container mx-auto px-4 py-8 text-center">
         <p>&copy; 2022 Тектоника. Все права защищены.</p>
-        <p className="text-sm text-gray-400 mt-2">Строительство каркасных домов в г. Чита.</p>
+        <p className="text-sm text-black mt-2">Строительство каркасных домов в г. Чита.</p>
       </div>
     </footer>
   );
