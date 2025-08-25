f8b722cf320598446e98cefa3bbcb53f2d599870
meit <zhora.makarov.2005@yandex.ru>
2025-08-25 14:14:56 +0900

    logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,10 @@ const Header: React.FC = () => {
   return (
     <header className="bg-white/80 backdrop-blur-md shadow-md sticky top-0 z-50">
       <div className="container mx-auto px-4 py-4 flex flex-col gap-2 sm:flex-row sm:justify-between sm:items-center">
-        <a href="#" className="text-2xl font-extrabold text-brand-dark text-center sm:text-left">
+        <div><img src="/public/img/logo.jpg" alt="Тектоника" className="w-16 h-16" /> <a href="#" className="text-2xl font-extrabold text-brand-dark text-center sm:text-left">
           Тектоника
-        </a>
+        </a></div>
+       
         <div className="text-center sm:text-right">
             <a href="tel:+79145216931" className="text-lg font-bold text-brand-blue hover:text-blue-900 transition-colors">
                 +7 (914) 521-69-31
