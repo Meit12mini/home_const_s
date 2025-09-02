b695da9b951c5c74cf19d548be06b2d6c26a61fb
meit <zhora.makarov.2005@yandex.ru>
2025-09-02 17:42:19 +0900

    fix scroll

--- App.tsx
+++ App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
         <WhyUs />
         <Gallery />
         <Testimonials />
-        <Contact />
+        <Contact onScrollToConstructor={handleScrollToConstructor}  />
       </main>
       <Footer />
     </div>
