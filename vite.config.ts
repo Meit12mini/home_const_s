0e9f3250d37e5502625e3f7e064e1c1351f6535a
meit <zhora.makarov.2005@yandex.ru>
2025-09-04 12:55:35 +0900

    nbnb

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -10,6 +11,13 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
+      ViteImageOptimizer({
+      // Настройки оптимизации
+      jpg: { quality: 50 },
+      png: { quality: 60 },
+      webp: { quality: 50 },
+      avif: { quality: 50 },
+    }),
       
     ],
     define: {
